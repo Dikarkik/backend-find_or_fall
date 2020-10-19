a9cc7f0cd1450dce4555036925c16bf6d6e98008
Diana Carolina Quintero <karodev3@gmail.com>
2020-10-19 18:30:18 -0500

    Update random_buttons to set strings instead ind to avoid errors with socketIO library in Unity

--- random_buttons.js
+++ random_buttons.js
@@ -1,6 +1,6 @@
-exports.generateBoard = function (room) {
-  room.button_1 = Math.floor(Math.random() * 8);
-  room.button_2 = Math.floor(Math.random() * 8);
+exports.randomButtons = function (room) {
+  room.button_1 = "" + Math.floor(Math.random() * 8);
+  room.button_2 = "" + Math.floor(Math.random() * 8);
   while (room.button_2 === room.button_1)
-    room.button_2 = Math.floor(Math.random() * 8);
+    room.button_2 = "" + Math.floor(Math.random() * 8);
 };
